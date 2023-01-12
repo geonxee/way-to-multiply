0e307a75311751d6a594c9df189760abcd74f7a2
sseungki98 <appp_le@naver.com>
2023-01-12 09:44:10 +0900

    feat: Get number and divide small number by 2
    And, put that numbers into list.

--- ethiopian.py
+++ ethiopian.py
@@ -1,5 +1,24 @@
-def hello():
-    print('Good luck!')
+def get_input():
+    num_1 = int(input("Enter the first number > "))
+    num_2 = int(input("Enter the second number > "))
+    return num_1, num_2
+
+def divide_by_2(number):
+    mocks = []
+    while number != 0:
+        mocks.append(number)
+        number = number // 2
+
+    return mocks
 
 if __name__=='__main__':
-    hello()
+    (num_1, num_2) = get_input()
+    mocks = []
+
+    if num_1 > num_2:
+        mocks = divide_by_2(num_2)
+    else:
+        mocks = divide_by_2(num_1)
+
+    print(mocks)
+    
